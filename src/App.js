eea4dd2b48798f4cbc08da98fb1220dc41e968fc
faruqismael <farusmith5@gmail.com>
2021-07-11 14:19:22 +0300

    redirecting / to some long url - react router

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route exact path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
